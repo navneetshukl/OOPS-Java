5ae7d0938af65cd80d4097ba3956889c39dbe2e8
navneetshukl <navneetshukla824@gmail.com>
2024-12-17 22:00:44 +0530

    multiple inheritance in java using interface

---
+++ Interface/multiple-inheritance/C.java
@@ -0,0 +1,11 @@
+public class C implements A,B{
+    @Override
+    public void execute1(){
+        System.out.println("Haii.. I am from execute1");
+    }
+    @Override
+    public void execute2()
+    {
+        System.out.println("Haii.. I am from execute2");
+    }
+}
---
+++ Interface/multiple-inheritance/main.java
@@ -0,0 +1,7 @@
+public class main{
+    public static void main(String[] args) {
+        C obj=new C();
+        obj.execute1();
+        obj.execute2();
+    }
+}
